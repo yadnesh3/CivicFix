5f9f52ef78e7da0adedf47322e0ee3baf2d8723b
yadnesh gharat <yadneshgharat23@gmail.com>
2026-08-28 00:23:34 +0530

    mongo url is connected

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("CivicFix Backend is running");
+});
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
